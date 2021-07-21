3157ef8ff4562ab4f948faf4059b0f7b5c0605d7
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-21 17:53:43 -0500

    update BEtty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using post-order
  * @tree: root
  * @func: ptr a function
  * Return: void
