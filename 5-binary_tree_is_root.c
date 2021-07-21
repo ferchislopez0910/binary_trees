39d46f8ea22bd34b22bd325d1e8b5d1b11307d87
mareizaa <marce.3210@gmail.com>
2021-07-21 13:08:17 -0500

    betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - function that inserts a node as the left-child
+ * binary_tree_is_root -  checks if a given node is a root
  * @node: pointer to the node to check
  * Return: 1 if node is a leaf, otherwise 0
  */
