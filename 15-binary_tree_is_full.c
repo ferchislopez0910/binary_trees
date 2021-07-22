5a9f8b85905e2f43110033bc5883e3bb2595b0f4
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-22 00:51:08 -0500

    update: Betty!

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - counts the leaves in a binary tree
+ * binary_tree_is_full - function that checks if a binary tree is full
  * @tree: root
  * Return: number leaves
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-        return (0);
-    if (!tree->left && !tree->right)
-    {
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    }
-    return (0);
+		return (0);
+	if (!tree->left && !tree->right)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+	return (0);
 }
