2b2e95674bf10ed82039156b0b0b7f921c4a739d
mareizaa <marce.3210@gmail.com>
2021-07-21 21:55:33 -0500

    ternarios

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,23 +6,9 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-	{
-		count++;
-	}
-	if (tree->right)
-	{
-		count++;
-	}
-	if (!tree->right && !tree->left)
-	{
-		return (1);
-	}
-
-	return (count);
+	return (!tree->left && !tree->right ? 1:\
+	binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
