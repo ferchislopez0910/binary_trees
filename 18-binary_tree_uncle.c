f03b7a891dee6504cd3bb2deb3b6c5c2958cd2cf
mareizaa <marce.3210@gmail.com>
2021-07-22 01:27:09 -0500

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - counts the leaves in a binary tree
+ * @node: pointer to the node to find the sibling
+ * Return: number leaves
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    binary_tree_t *uncle_l;
+
+	if (!node || !node->parent)
+	{
+		return (NULL);
+	}
+
+    uncle_l = node->parent;
+
+    if (!node->parent->parent)
+    {
+        return (NULL);
+    }
+
+	if (node->parent == uncle_l->parent->left)
+	{
+		if (uncle_l->parent->right)
+		{
+			return (uncle_l->parent->right);
+		}
+		else
+			return (NULL);
+	}
+	else
+	{
+		if (uncle_l->parent->left)
+		{
+			return (uncle_l->parent->left);
+		}
+		return (NULL);
+	}
+}
