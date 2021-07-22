0801dc1b35e94ff7d1afa3433ec584e3107b4255
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-21 20:18:48 -0500

    update task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - goes through a binary tree using post-order
+ * @tree: root
+ * Return: void
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int count_l = 0;
+	int count_r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	if (count_l > count_r)
+	{
+		return (binary_tree_height(tree->left) + 1);
+	}
+	else
+	{
+		return (binary_tree_height(tree->right) + 1);
+	}
+}
