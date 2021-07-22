c74ebad59febaedd524474998e5ba402c507dca4
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-21 21:58:34 -0500

    update: Betty 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,6 +9,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (!tree->left && !tree->right ? 1:\
+	return (!tree->left && !tree->right ? 1 :
 	binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
